dea6160e704db3a72a904c17df7d214d4098573a
Artem Rumiantcev <tema.place@gmail.com>
2019-04-11 13:53:23 +0300

    GH-19 fix csv report save only http https and reletive links

--- lib/browser_crawler/reports/csv_report.rb
+++ lib/browser_crawler/reports/csv_report.rb
@@ -1,5 +1,5 @@
 require 'csv'
-
+require 'pry'
 module BrowserCrawler
   module Reports
     # It involves methods which allow to save a store to a csv file
@@ -10,35 +10,62 @@ module BrowserCrawler
 
       def export(save_folder_path:)
         CSV.open("#{save_folder_path}/crawler_report.csv", 'wb') do |csv|
-          csv << ['pages',
-                  'extracted links',
-                  'external?',
-                  'http status',
-                  'http code']
+          csv << csv_header
 
           @store.pages.each do |page, crawler_result|
-            status = humanize_code(crawler_result[:code])
-            if crawler_result[:extracted_links].nil?
-              csv << [page,
-                      nil,
-                      crawler_result[:external],
-                      status,
-                      crawler_result[:code]]
-              next
-            end
-
-            crawler_result[:extracted_links].each do |link|
-              csv << [page,
-                      link,
-                      crawler_result[:external],
-                      status,
-                      crawler_result[:code]]
-            end
+            save_to_csv(csv, page, crawler_result)
           end
         end
       end
 
       private
+
+      def filter_links(links)
+        return nil if links.nil?
+
+        links.select do |link|
+          begin
+            uri = URI(link)
+          rescue URI::InvalidURIError, URI::InvalidComponentError
+            next
+          end
+
+          if uri.scheme
+            link =~ /\A#{URI.regexp(%w[http https])}\z/
+          else
+            true
+          end
+        end
+      end
+
+      def save_to_row(page, crawler_result, link = nil)
+        [page,
+         link,
+         crawler_result[:external],
+         humanize_code(crawler_result[:code]),
+         crawler_result[:code]]
+      end
+
+      def save_to_csv(csv, page, crawler_result)
+        extracted_links = filter_links(crawler_result[:extracted_links])
+
+        if extracted_links.nil?
+          csv << save_to_row(page, crawler_result)
+          return
+        end
+
+        extracted_links.each do |link|
+          csv << save_to_row(page, crawler_result, link)
+        end
+      end
+
+      def csv_header
+        ['pages',
+         'extracted links',
+         'external?',
+         'http status',
+         'http code']
+      end
 
       def humanize_code(code)
         case code.to_i
